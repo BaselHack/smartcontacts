7db75a2299a642aebff8c014a96f364c76be5244
Sebastian Fix <sebastian.fix@smartgate.ch>
2017-10-29 13:48:30 +0100

    Updated

--- app/Http/Requests/StoreContactRequest.php
+++ app/Http/Requests/StoreContactRequest.php
@@ -27,7 +27,7 @@ class StoreContactRequest extends FormRequest
 
             'firstname' => 'required|string|max:60',
             'lastname' => 'required|string|max:60',
-            'email' => 'required|email|max:100',
+            'email' => 'required|email|max:100|unique:contacts',
             'mobile' => 'required|numeric'
         ];
     }
