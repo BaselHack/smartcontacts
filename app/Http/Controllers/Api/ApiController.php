f7ab23390ffa62c30ca6666e8b8eb4d0f35b78d4
Sebastian Fix <sebastian.fix@smartgate.ch>
2017-10-29 14:14:32 +0100

    Changes API

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Contact;
 use App\Http\Controllers\Controller;
 use App\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -21,10 +22,35 @@ class ApiController extends Controller
             $friend = User::where('email', '=', $friend)->first();
 
             $contact = Contact::where('user_id','=',$friend->id)->first();
-            
+
             $user = User::where('email', '=', $email)->first();
 
             $user->hasManyContacts()->attach($contact);
+
+            if (env('CARDDAV_STATUS')) {
+
+                try
+                {
+
+                    $client = new Client([
+                        'headers' => [ 'Content-Type' => 'application/json' ]
+                    ]);
+
+                    $response = $client->post(env('CARDDAV_URL') . 'api/make_friends',
+                        ['body' => json_encode(
+                            [
+                                'email' => $user->email,
+                                'uuid' => $contact->uuid,
+                            ]
+                        )]
+                    );
+
+                }
+                catch (\Exception $exception)
+                {
+
+                }
+            }
 
             return redirect()->route('frontend.makefriend.success');
 
